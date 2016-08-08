4073255350423fe0daf85123d348802ccd05ee20
Bharathi1310 <bharathikalai1310@gmail.com>
2016-08-08 02:33:47 -0700

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Conversion {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the value: ");
+	String s=in.nextLine();
+	int n=Integer.parseInt(s);
+	System.out.println("Integer : "+n);
+	}
+
+}
